ff6334fbc99a8728a1edc387a15c116e6acb656d
Michał Warda <michal.warda@elpassion.pl>
2022-03-24 16:01:57 +0100

    Introduce `withRecording` helper method

--- src/currencies.test.ts
+++ src/currencies.test.ts
@@ -1,11 +1,18 @@
-import nock from "nock";
-import { resolve } from "path";
 import { getCurrencyData } from "./getCurrencyData";
+import { withRecording } from "./test-utils/withRecording";
 
 describe("currenciesApi", () => {
-  it("works", async () => {
-    nock.load(resolve(__dirname, "__tapes__", "getCurrencies.json"));
-    const data = await getCurrencyData();
-    expect(data.rates[0].bid).toBe(3.6929);
+  it("returns correct currencies for today", async () => {
+    await withRecording(__dirname, "kaseta", async () => {
+      const data = await getCurrencyData();
+      expect(data.rates[0].bid).toBe(3.6929);
+    });
+  });
+
+  it("returns correct currencies for today", async () => {
+    await withRecording(__dirname, "kaseta2", async () => {
+      const data = await getCurrencyData();
+      expect(data.rates[0].bid).toBe(3.6929);
+    });
   });
 });
